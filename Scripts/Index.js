9ee0650c035fd90a20303748b7ce6dbac3c43540
Aryan <ironmantknystark00@gmail.com>
2020-04-15 16:37:18 -0700

    Normal Commit

--- Scripts/Index.js
+++ Scripts/Index.js
@@ -6,3 +6,9 @@ function hide_menu(){
     var x=document.getElementById("menu");
     x.style.display = "none";
 }
+function hide_arrow(){
+    document.getElementById("arrow1").style.opacity = "0";
+}
+function show_arrow(){
+    document.getElementById("arrow1").style.opacity = "1";
+}
